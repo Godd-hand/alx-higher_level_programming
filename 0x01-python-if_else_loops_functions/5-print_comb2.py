affc5f6ef281602e6175701a391d283c3742f53b
God hand <interstellar300000000@gmail.com>
2022-06-28 04:08:11 -0700

    A number that prints numbers from 0 to 99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+able File  6 lines (6 sloc)  162 Bytes
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
