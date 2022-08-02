80ef814b5baf25022e0bdebb0425bff5c5803065
God hand <interstellar300000000@gmail.com>
2022-08-02 12:46:02 -0700

    Updates to 7-add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""
-A json Script that incorporates
-Modules from task 5 & 6
-it uses function from module 5 to save
-command line arguement to the list jsonList
-and uses fumction from module 6 to print the list
-"""
+"""add item"""
 
 
 import json
 import sys
 import os.path
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-i = len(sys.argv)
-jsonList = []
-for a in range(1, i):
-    jsonList.append(sys.argv[a])
-
-save_to_json_file(jsonList, 'add_item.json')
-load_from_json_file('add_item.json')
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
