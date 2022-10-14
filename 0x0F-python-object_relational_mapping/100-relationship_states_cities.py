6196bb1b76b186d31ae959885f49b64d3ad27bef
God hand <interstellar300000000@gmail.com>
2022-10-14 13:41:03 -0700

    solving the 15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-""" Script that creates the State “California” with the City
-    “San Francisco” from the database hbtn_0e_100_usa """
+"""
+All states via SQLAlchemy
+"""
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-
-    import sys
-    from relationship_state import State, Base
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    s = Session(engine)
-    news = State(name='California')
-    newc = City(name='San Francisco')
-    news.cities.append(newc)
-    s.add_all([news, newc])
-    s.commit()
-    s.close()
+    session = Session(engine)
+    new_state = State(name='California')
+
+    new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
+
+    session.add(new_state)
+    session.commit()
+    session.close()
