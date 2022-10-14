94b9c415f178133cb47f9d23b804a59676e5652e
God hand <interstellar300000000@gmail.com>
2022-10-14 13:38:50 -0700

    changes to city

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-'''task 15 model script'''
+"""
+class definition of a State and an instance Base = declarative_base()
+"""
 
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from relationship_state import Base
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class City(Base):
-    '''City model for my db'''
+    """
+    City class:
+    inherits from Base
+    links to the MySQL table states
+    class attribute id that represents a column of an auto-generated,
+    unique integer, cant be null and is a primary key
+    class attribute name that represents a column of a string
+    with maximum 128 characters and cant be null
+    class attribute state_id that represents a column of an integer,
+    can’t be null and is a foreign key to states.id
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, unique=True, nullable=False,
-                autoincrement=True, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship('State', back_populates='cities')
-~
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
